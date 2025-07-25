c5298a52d2a4053c6b7b9acab5d0bcd48e830e2c
Yonaki97 <yogue97@gmail.com>
2025-07-26 00:20:19 +0700

    Final

--- tugasbesar_rpl/backend/Config/ConnectDb.js
+++ tugasbesar_rpl/backend/Config/ConnectDb.js
@@ -1,69 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/Reskom_db', {
+    await mongoose.connect('mongodb+srv://yogue97:x9zj7EjqUrRJJYTg@reskom.mkmp5yx.mongodb.net/Reskom?retryWrites=true&w=majority&appName=Reskom', {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected');
+    console.log("MongoDB Atlas Connected ✅");
   } catch (err) {
-    console.error(err.message);
-    process.exit(1); // keluar kalau gagal
+    console.error("❌ MongoDB connection failed:", err.message);
+    process.exit(1);
   }
 };
 
 module.exports = connectDB;
-
-// const transporter = nodemailer.createTransport({
-//   service: 'Gmail',
-//   auth: {
-//     user: 'blackleaner@gmail.com',
-//     pass: '9087'
-//   }
-// });
-
-// app.post('/ForgotPassword', async (req, res) => {
-//   const { email } = req.body;
-//   const user = await User.findOne({ email });
-
-//   if (!user) return res.json({ message: 'Email tidak ditemukan' });
-
-//   const token = crypto.randomBytes(20).toString('hex');
-//   user.resetToken = token;
-//   user.tokenExpires = Date.now() + 3600000; // 1 jam
-//   await user.save();
-
-//   const link = `http://localhost:5173/ResetPassword/${token}`;
-
-//   // Kirim email
-//   try{
-//     await transporter.sendMail({
-//     to: user.email,
-//     subject: "Reset Password",
-//     html: `<a href="${link}">Klik untuk reset password</a>`
-//   });
-
-//   res.json({ message: 'Link reset dikirim ke email' });
-// }catch (error) {
-//     console.error("Gagal mengirim email:", error);
-//     res.status(500).json({ message: "Gagal mengirim email", error: error.message });
-//   }
-// });
-
-
-// app.post('/ResetPassword', async (req, res) => {
-//   const { token, password } = req.body;
-//   const user = await User.findOne({
-//     resetToken: token,
-//     tokenExpires: { $gt: Date.now() } // Token masih valid
-//   });
-
-//   if (!user) return res.json({ message: 'Token tidak valid atau expired' });
-
-//   user.password = hash(password); // hash sesuai sistemmu
-//   user.resetToken = undefined;
-//   user.tokenExpires = undefined;
-//   await user.save();
-
-//   res.json({ message: 'Password berhasil direset' });
-// });
